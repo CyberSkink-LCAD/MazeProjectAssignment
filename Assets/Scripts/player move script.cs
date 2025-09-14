79a08160934805721c839c51e4fd1d24efc84db5
itmecooper <166541443+itmecooper@users.noreply.github.com>
2025-09-13 20:33:56 -0700

    added my maze
    coopers maze with a working win script added in as a independent scene

---
+++ Assets/Scripts/player move script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovescript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed =20f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v);
+        transform.Translate(move * speed * Time.deltaTime);
+   }
+   
+
+}
